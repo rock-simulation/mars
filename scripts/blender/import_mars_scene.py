5d5c71a0d36ae938ed51b7f508a675057aaa73cb
Lorenz Quack <lorenz.quack@dfki.de>
2013-03-15 13:00:31 +0100

    blender import script: made code more pythonic.

--- scripts/blender/import_mars_scene.py
+++ scripts/blender/import_mars_scene.py
@@ -1,4 +1,8 @@
 import os
+import sys
+import shutil
+import ZipFile
+import textwrap
 
 import xml.dom.minidom
 
@@ -7,28 +11,29 @@ def getGenericConfig(domElement):
     config = {}
 
     if domElement.hasAttributes():
-        attributes = domElement.attributes
-        for (key,value) in attributes.items():
+        for key, value in domElement.attributes.items():
 #            print "attrib [%s : %s]" % (key, value)
-            if key not in config.keys():
+            if key not in config:
                 config[key] = value
             else:
-                print "Warning! Key \'%s\' already exists!" % key
+                print("Warning! Key '%s' already exists!" % key)
 
     if domElement.hasChildNodes():
-        children = domElement.childNodes
-        for child in children:
+        for child in domElement.childNodes:
             key   = child.nodeName
             value = child.nodeValue
             if child.hasChildNodes():
                 value = getGenericConfig(child)
 #                print "element [%s : %s]" % (key, value)
-                if key not in config.keys():
+                if key not in config:
                     config[key] = value
                 else:
-                    print "Warning! Key \'%s\' already exists!" % key
+                    print("Warning! Key '%s' already exists!" % key)
             else:
-                value = ''.join(value.split())
+                # remove indentation and leading and trailing whitespaces
+                value = textwrap.dedent(value).strip()
+#                value = ''.join(value.split())
+                ## FIXME: This seems wrong! what about the other cildren?
                 if value != '':
                     return value
 
@@ -41,33 +46,31 @@ def parseNode(domElement):
     return True
 
 
-def main():
-    filepath = "/Users/bergatt/Desktop/import_mars_scene"
-    filename = "aila_clean.scn"
-
+def main(fileDir, filename):
+    tmpDir = os.path.join(fileDir, "tmp")
+    
     # change the current directory
-    if os.path.isdir(filepath) :
-        os.chdir(filepath)
+    if os.path.isdir(fileDir) :
+        os.chdir(fileDir)
     else:
-        print "ERROR! File path (%s) does not exist!" % filepath
-        exit()
+        print("ERROR! File path (%s) does not exist!" % fileDir)
+        sys.exit(1)
 
     # if there is already a "tmp" directory delete it
-    if os.path.isdir(filepath+os.sep+"tmp") :
-        os.system("rm -rf tmp")
+    shutil.rmtree(tmpDir, ignore_errors=True)
     
     # extract the .scn file to the "tmp" directory
-    os.system("unzip %s -d tmp" % filename)
+    ZipFile(os.path.join(fileDir, filename), "r").extractall(tmpDir)
 
     # path to the .scene file
-    scenepath = filepath+os.sep+"tmp"+os.sep+filename.replace('.scn','.scene')
+    scenepath = os.path.join(tmpDir, filename.replace('.scn', '.scene'))
 
     # if the .scene file exists, read all of its content
     if os.path.isfile(scenepath) :
         dom = xml.dom.minidom.parse(scenepath)
     else:
-        print "ERROR! Couldn't find .scene file (%s)!" % scenepath
-        exit()
+        print("ERROR! Couldn't find .scene file (%s)!" % scenepath)
+        sys.exit(1)
 
     # --- DO THE PARSING HERE!!! ---
     
@@ -75,11 +78,20 @@ def main():
     nodes = dom.getElementsByTagName("node")
     for node in nodes :
         if not parseNode(node):
-            print "Error while parsing parsing node!"
-            exit()
+            print("Error while parsing parsing node!")
+            sys.exit(1)
 
     #cleaning up afterwards
-    os.system("rm -rf tmp")
+    shutil.rmtree(tmpDir)
+
 
 if __name__ == '__main__' :
-    main()
+    if len(sys.argv) != 2:
+        print("USAGE: %s <fullpath_to_scn_file>" % sys.argv[0])
+        sys.exit(1)
+    if not os.path.isfile(sys.argv[1]):
+        print('USAGE: %s <fullpath_to_scn_file>' % sys.argv[0])
+        print('  Error: "%s" is not an existing file!' % sys.argv[1])
+        sys.exit(1)
+    fileDirectory, filename = os.path.split(sys.argv[1])
+    main(fileDirectory, filename)
