0c7a11e320ecc3ab44f3526446504131a40ccdb0
Constantin Bergatt <bergatt@Bergatt-MacPro.local>
2013-03-15 14:52:01 +0100

    * merged with Lorenz changes & added new functionality for parsing node data

--- scripts/blender/import_mars_scene.py
+++ scripts/blender/import_mars_scene.py
@@ -1,10 +1,23 @@
 import os
 import sys
 import shutil
-import ZipFile
+import zipfile
 import textwrap
 
 import xml.dom.minidom
+
+# allowed node types
+nodeTypes = ["undefined",
+             "mesh",
+             "box",
+             "sphere",
+             "capsule",
+             "cylinder",
+             "plane",
+             "terrain",
+             "reference"
+            ]
+
 
 def getGenericConfig(domElement):
 
@@ -40,15 +53,246 @@ def getGenericConfig(domElement):
     return config
 
 
+def checkConfigParameter(config,key):
+    if key not in config.keys():
+        print("WARNING! Node config does not contain parameter \'%s\'!" % key)
+        return False
+    return True
+
+
 def parseNode(domElement):
+    # read the config from the xml file
     config = getGenericConfig(domElement)
-#    print config["name"], ":", config
+#    print(config["name"], ":", config)
+
+    check = False
+    massDensity = False
+    needMass = True
+
+    # handle node name
+    if not checkConfigParameter(config,"name"):
+        return False
+    name = config["name"]
+
+    # handle node mass
+    if checkConfigParameter(config,"mass"):
+        mass = float(config["mass"])
+        # use some epsilon here
+        if abs(mass) > 0.000000001:
+            check = True
+
+    if checkConfigParameter(config,"density"):
+        density = float(config["density"])
+        # use some epsilon here
+        if abs(density) > 0.000000001:
+            if check:
+                massDensity = True;
+            check = True;
+
+    if checkConfigParameter(config,"noPhysical"):
+        noPhysical = (config["noPhysical"] == "true")
+        if noPhysical:
+            needMass = False
+
+    if checkConfigParameter(config,"movable"):
+        movable = (config["movable"] == "true")
+        if not movable:
+            needMass = False
+
+    if needMass:
+        if not check:
+            print("WARNING! No mass nor density given for node %s." % name)
+        elif massDensity:
+            print("WARNING! Mass and density given for node %s. Using only the mass." % name)
+            density = 0.0
+
+    # handle node physic mode
+    if checkConfigParameter(config,"physicmode"):
+        typeName = config["physicmode"].strip()
+        if typeName in nodeTypes:
+            physicMode = nodeTypes.index(typeName)
+        else:
+            print("ERROR! Could not get type for node: %s" % name)
+
+    if checkConfigParameter(config,"origname"):
+        origName = config["origname"].strip()
+
+    if checkConfigParameter(config,"filename"):
+        filename = config["filename"].strip()
+
+    if filename == "PRIMITIVE":
+        if not origName:
+            origName = nodeTypes[physicMode]
+        elif origName != nodeTypes[physicMode]:
+            tmp = nodeTypes[physicMode]
+            print("WARNING! Origname set to \"%s\" for primitive in node \"%s\" with physicMode \"%s\"" % (origName, name, tmp))
+
+    if checkConfigParameter(config,"groupid"):
+        groupID = int(config["groupid"])
+
+    if checkConfigParameter(config,"index"):
+        index = long(config["index"])
+
+    if checkConfigParameter(config,"position"):
+        pos = config["position"]
+
+    if checkConfigParameter(config,"pivot"):
+        pivot = config["pivot"]
+
+    if checkConfigParameter(config,"rotation"):
+        rot = config["rotation"]
+
+    if checkConfigParameter(config,"extend"):
+        ext = config["extend"]
+
+    # handle relatvie positioning
+    if checkConfigParameter(config,"relativeid"):
+        relative_id = long(config["relativeid"])
+        if relative_id:
+            if checkConfigParamter(config,"mapIndex"):
+                mapIndex = int(config["mapIndex"])
+            #TODO: Do we need this?
+            #if mapIndex and loadScene:
+            #    relative_id = loadScene->getMappedID(relative_id, MAP_TYPE_NODE, mapIndex);
+
+    # handle terrain info
+    if checkConfigParameter(config,"t_srcname"):
+      terrain = {}
+      terrain["srcname"] = config["t_srcname"]
+
+      if checkConfigParameter(config,"t_width"):
+          terrain["targetWidth"] = float(config["t_width"])
+
+      if checkConfigParameter(config,"t_height"):
+          terrain["targetHeight"] = float(config["t_height"])
+
+      if checkConfigParameter(config,"t_scale"):
+          terrain["scale"] = float(config["t_scale"])
+
+      if checkConfigParameter(config,"t_tex_scale"):
+          terrain["texScale"] = float(config["t_tex_scale"])
+
+    if checkConfigParameter(config, "visualposition"):
+        visual_offset_pos = config["visualposition"]
+
+    if checkConfigParameter(config, "visualrotation"):
+        visual_offset_rot = config["visualrotation"]
+
+    if checkConfigParameter(config,"visualsize"):
+        visual_size = config["visualsize"]
+    else:
+      visual_size = ext
+
+    # handle contact info
+    c_params = {}
+
+    if checkConfigParameter(config,"cmax_num_contacts"):
+        c_params["max_num_contacts"] = float(config["cmax_num_contacts"])
+        
+    if checkConfigParameter(config,"cerp"):
+        c_params["erp"] = float(config["cerp"])
+        
+    if checkConfigParameter(config,"ccfm"):
+        c_params["cfm"] = float(config["ccfm"])
+
+    if checkConfigParameter(config,"cfriction1"):
+        c_params["friction1"] = float(config["cfriction1"])
+
+    if checkConfigParameter(config,"cfriction2"):
+        c_params["friction2"] = float(config["cfriction2"])
+
+    if checkConfigParameter(config,"cmotion1"):
+        c_params["motion1"] = float(config["cmotion1"])
+
+    if checkConfigParameter(config,"cmotion2"):
+        c_params["motion2"] = float(config["cmotion2"])
+
+    if checkConfigParameter(config,"cfds1"):
+        c_params["fds1"] = float(config["cfds1"])
+
+    if checkConfigParameter(config,"cfds2"):
+        c_params["fds2"] = float(config["cfds2"])
+
+    if checkConfigParameter(config,"cbounce"):
+        c_params["bounce"] = float(config["cbounce"])
+
+    if checkConfigParameter(config,"cbounce_vel"):
+        c_params["bounce_vel"] = float(config["cbounce_vel"])
+
+    if checkConfigParameter(config,"capprox"):
+        c_params["approx_pyramid"] = (config["capprox"] == "true")
+
+    if checkConfigParameter(config,"coll_bitmask"):
+        c_params["coll_bitmask"] = int(config["coll_bitmask"])
+
+    if checkConfigParameter(config,"cfdir1"):
+        c_params["friction_direction1"] = config["cfdir1"]
+
+    if checkConfigParameter(config,"inertia"):
+        inertia_set = (config["inertia"] == "true")
+
+    inertia = [[0.0 for x in range(3)] for x in range(3)]
+
+    if checkConfigParameter(config,"i00"):
+        inertia[0][0] = float(config["i00"])
+
+    if checkConfigParameter(config,"i01"):
+        inertia[0][1] = float(config["i01"])
+
+    if checkConfigParameter(config,"i02"):
+         inertia[0][2] = float(config["i02"])
+
+    if checkConfigParameter(config,"i10"):
+         inertia[1][0] = float(config["i10"])
+
+    if checkConfigParameter(config,"i11"):
+         inertia[1][1] = float(config["i11"])
+
+    if checkConfigParameter(config,"i12"):
+         inertia[1][2] = float(config["i12"])
+
+    if checkConfigParameter(config,"i20"):
+         inertia[2][0] = float(config["i20"])
+
+    if checkConfigParameter(config,"i21"):
+         inertia[2][1] = float(config["i21"])
+
+    if checkConfigParameter(config,"i22"):
+         inertia[2][2] = float(config["i22"])
+
+    if checkConfigParameter(config,"linear_damping"):
+         linear_damping = float(config["linear_damping"])
+
+    if checkConfigParameter(config,"angular_damping"):
+         angular_damping = float(config["angular_damping"])
+
+    if checkConfigParameter(config,"angular_low"):
+         angular_low = float(config["angular_low"])
+
+    if checkConfigParameter(config,"shadow_id"):
+         shadow_id = int(config["shadow_id"])
+
+    if checkConfigParameter(config,"shadowcaster"):
+         isShadowCaster = (config["shadowcaster"] == "true")
+
+    if checkConfigParameter(config,"shadowreceiver"):
+         isShadowReceiver= (config["shadowreceiver"] == "true")
+
+    #TODO: Do we need this?
+    #if(!filenamePrefix.empty()) { 
+    #   if(filename != "PRIMITIVE")
+    #     handleFilenamePrefix(&filename, filenamePrefix);
+    #   if(terrain) {
+    #     handleFilenamePrefix(&terrain->srcname, filenamePrefix);
+    #   }
+    # }
+
     return True
 
 
 def main(fileDir, filename):
     tmpDir = os.path.join(fileDir, "tmp")
-    
+
     # change the current directory
     if os.path.isdir(fileDir) :
         os.chdir(fileDir)
@@ -60,7 +304,7 @@ def main(fileDir, filename):
     shutil.rmtree(tmpDir, ignore_errors=True)
     
     # extract the .scn file to the "tmp" directory
-    ZipFile(os.path.join(fileDir, filename), "r").extractall(tmpDir)
+    zipfile.ZipFile(os.path.join(fileDir, filename), "r").extractall(tmpDir)
 
     # path to the .scene file
     scenepath = os.path.join(tmpDir, filename.replace('.scn', '.scene'))
