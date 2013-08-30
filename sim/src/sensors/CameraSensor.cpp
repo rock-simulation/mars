e5a8cc82e7db90747bfa9f89767849e7e1d6f208
Sascha Arnold <sascha.arnold@dfki.de>
2013-08-30 18:01:50 +0200

    use camera configuration to set the opening angle of the camera

--- sim/src/sensors/CameraSensor.cpp
+++ sim/src/sensors/CameraSensor.cpp
@@ -98,7 +98,7 @@ namespace mars {
         if(gw) {
           gc = gw->getCameraInterface();
           control->graphics->addGraphicsUpdateInterface(this);
-          //gc->setFrustumFromRad(150.0/180.0*M_PI,90.0/180.0*M_PI,0.5,100);
+          gc->setFrustumFromRad(config.opening_width/180.0*M_PI,config.opening_height/180.0*M_PI,0.5,100);
         }
       }
     }
