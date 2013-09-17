6b40946fab6ebbcd41102e6e3d2046ba96e0752c
Janosch Machowinski <Janosch.Machowinski@dfki.de>
2013-09-18 00:55:43 +0200

    Reimplemented RaySensor to use rttDepth image instead of ODE rays
    This is still eperimental but seems to work.
    It might be that the returned array of distance values
    is mirrored now.

--- sim/src/sensors/RaySensor.cpp
+++ sim/src/sensors/RaySensor.cpp
@@ -33,162 +33,310 @@
 #include <mars/interfaces/sim/LoadSceneInterface.h>
 
 #include <mars/data_broker/DataBrokerInterface.h>
-#include <mars/interfaces/graphics/GraphicsManagerInterface.h>
+#include <mars/utils/mathUtils.h>
 
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
 
 namespace mars {
-  namespace sim {
+namespace sim {
 
-    using namespace utils;
-    using namespace interfaces;
+using namespace utils;
+using namespace interfaces;
 
-    BaseSensor* RaySensor::instanciate(ControlCenter *control, BaseConfig *config ){
-      RayConfig *cfg = dynamic_cast<RayConfig*>(config);
-      assert(cfg);
-      return new RaySensor(control,*cfg);
-    }
+BaseSensor* RaySensor::instanciate(ControlCenter *control, BaseConfig *config ){
+    RayConfig *cfg = dynamic_cast<RayConfig*>(config);
+    assert(cfg);
+    return new RaySensor(control,*cfg);
+}
 
-    RaySensor::RaySensor(ControlCenter *control, RayConfig config): 
-      BasePolarIntersectionSensor(config.id, config.name, config.width,
-                                  config.height, config.opening_width,
-                                  config.opening_height),
-      SensorInterface(control), config(config) {
+RaySensor::RaySensor(ControlCenter *control, RayConfig config): 
+    BaseNodeSensor(config.id, config.name),
+    SensorInterface(control), config(config) {
 
-      updateRate = 0;
-      orientation.setIdentity();
-      maxDistance = config.maxDistance;
-      this->attached_node = config.attached_node;
+    updateRate = 0;
+    long attached_node = config.attached_node;
 
-      std::string groupName, dataName;
-      drawStruct draw;
-      draw_item item;
-      int i;
-      Vector tmp;
-      have_update = false;
-      for(int i = 0; i < 3; ++i)
-        positionIndices[i] = -1;
-      for(int i = 0; i < 4; ++i)
-        rotationIndices[i] = -1;
+    std::string groupName, dataName;
+    drawStruct draw;
+    draw_item item;
+    int i;
+    Vector tmp;
+    for(int i = 0; i < 3; ++i)
+	positionIndices[i] = -1;
+    for(int i = 0; i < 4; ++i)
+	rotationIndices[i] = -1;
 
-      control->nodes->addNodeSensor(this);
-      bool erg = control->nodes->getDataBrokerNames(attached_node, &groupName, &dataName);
-      assert(erg);
-      if(control->dataBroker->registerTimedReceiver(this, groupName, dataName,"mars_sim/simTimer",updateRate)) {
-      }
-
-      position = control->nodes->getPosition(attached_node);
-      orientation = control->nodes->getRotation(attached_node);
-
-      //Drawing Stuff
-      if(config.draw_rays) {
-        draw.ptr_draw = (DrawInterface*)this;
-        item.id = 0;
-        item.type = DRAW_LINE;
-        item.draw_state = DRAW_STATE_CREATE;
-        item.point_size = 1;
-        item.myColor.r = 1;
-        item.myColor.g = 0;
-        item.myColor.b = 0;
-        item.myColor.a = 1;
-        item.texture = "";
-        item.t_width = item.t_height = 0;
-        item.get_light = 0.0;
-          
+    control->nodes->addNodeSensor(this);
+    bool erg = control->nodes->getDataBrokerNames(attached_node, &groupName, &dataName);
+    assert(erg);
     
-        double rad_steps = getCols(); //rad_angle/(sReal)(sensor.resolution-1);
-        double rad_start = -((rad_steps-1)/2.0)*stepX; //Starting to Left, because 0 is in front and rock convention posive CCW //(M_PI-rad_angle)/2;
-        if(rad_steps == 1){
-          rad_start = 0;
-        }
+    if(control->dataBroker->registerTimedReceiver(this, groupName, dataName,"mars_sim/simTimer",updateRate)) {
+    }    
 
-        //printf("Rad Start: %f, rad_steps: %f, stepX: %f\n",rad_start,rad_steps,stepX);
+    if(control->graphics) {
+	/**
+	* We need to calculate the width and height in a way
+	* that the sensor error is not to big. This means we
+	* try not to cover at more than 1x1cm at the maximum distance
+	* with one pixel
+	* */
+	
+	double openingAngleForOneCmAtMaxDist = atan2(0.005, config.maxDistance);
+	
+	//HACK set a 'good' height
+	config.opening_height = openingAngleForOneCmAtMaxDist;
+	
+	std::cout << "Max dist " << config.maxDistance << " openingAngleForOneCmAtMaxDist " << openingAngleForOneCmAtMaxDist << std::endl;
+	
+	double anglePerCamera = M_PI /2.0;
+	
+	int numCameras = config.opening_width / anglePerCamera + 1; 
+	
+	subSensors.resize(numCameras);
+	
+	double coveredWidth = 0;
+	overallRttWidth = 0;
+	
+	for(int i = 0; i < numCameras; i++)
+	{
+	    std::cout << "Subcamera " << i << std::endl;
+	    
+	    //create multiple cameras to cover the width of the sensor
+	    double curWidth = std::min(anglePerCamera, config.opening_width - coveredWidth);
 
-        for(i=0; i<rad_steps; i++){
-          tmp = Vector(cos(rad_start+i*stepX),
-                       sin(rad_start+i*stepX), 0);
-          directions.push_back(tmp);
-          tmp = (orientation * tmp);
-          item.start = position;
-          item.end = (orientation * tmp);
-          item.end *= data[i];
-          draw.drawItems.push_back(item); // TEST
-        }
+	    //compute the configured min amount of pixels
+	    int minPixels = config.width * curWidth / config.opening_width;
+	    //amout of pixels to get a 'good' sensor resolution at max distance
+	    int neededPixels = curWidth / openingAngleForOneCmAtMaxDist;
+	    
+	    int rttWidth = std::max(minPixels, neededPixels);
+	    int rttHeight = std::max<int>(config.height, config.opening_height / openingAngleForOneCmAtMaxDist);
+
+	    std::cout << "Computing width an height for laser depth image to " << rttWidth << " " << rttHeight << std::endl; 
+
+	    long cam_window_id = control->graphics->new3DWindow(0, true, rttWidth, rttHeight, name);
+
+// 	    interfaces::hudElementStruct hudCam;
+// 	    hudCam.type            = HUD_ELEMENT_TEXTURE;
+// 	    hudCam.width           = 600;
+// 	    hudCam.height          = 300;
+// 	    hudCam.texture_width   = rttWidth;
+// 	    hudCam.texture_height  = rttHeight;
+// 	    hudCam.view_width      = hudCam.width;
+// 	    hudCam.view_height     = hudCam.height;
+// 	    hudCam.posx            = 40 + (hudCam.width * i); // aligned in a row
+// 	    hudCam.posy            = 300;
+// 	    hudCam.border_color[0] = 0.0;
+// 	    hudCam.border_color[1] = 0.58824;
+// 	    hudCam.border_color[2] = 0.0;
+// 	    hudCam.border_color[3] = 1.0;
+// 	    hudCam.border_width    = 5.0;
+// 
+// 	    long cam_id = control->graphics->addHUDElement(&hudCam);
+// 	    control->graphics->setHUDElementTextureRTT(cam_id, cam_window_id,false);
+
+	    
+	    interfaces::GraphicsWindowInterface *gw = control->graphics->get3DWindow(cam_window_id);
+	    assert(gw);
+	    interfaces::GraphicsCameraInterface *gc = NULL;
+	    gw->setGrabFrames(false);
+	    if(gw) {
+		gc = gw->getCameraInterface();
+		assert(gc);
+		control->graphics->addGraphicsUpdateInterface(this);
+		
+		std::cout << "Creating camera with opening width " << curWidth << " opening_height " << config.opening_height << std::endl;
+		
+		gc->setFrustumFromRad(curWidth, config.opening_height, 0.5, 100);
+	    }
+	    
+	    RaySubSensor *rs = &(subSensors[i]);
+	    rs->cam_window_id = cam_window_id;
+	    rs->gw = gw;
+	    rs->gc = gc;
+	    rs->rttWidth = rttWidth;
+	    rs->rttHeight = rttHeight;
+	    rs->depthBuffer.resize(rttHeight * rttWidth);
+	    rs->coveredAngle = curWidth;
+	    
+	    std::cout << " config.opening_width / 2. " << config.opening_width / 2.0 << " coveredWidth " << coveredWidth << " curWidth / 2.0 " << curWidth / 2.0 << std::endl; 
+	    
+	    double curAngle = config.opening_width / 2.0 - coveredWidth - curWidth / 2.0;
+	    
+	    std::cout << "Turnin subsensor to Angle " << curAngle << " / " << curAngle/M_PI * 180 << std::endl;
+	    
+	    //as for the * eulerToQuaternion(Vector(90,0,-90)) part
+	    //I have no idea why it is needed, but it fixes the orientation
+	    //of the camera.
+	    rs->orientation =  Eigen::AngleAxisd(curAngle, Eigen::Vector3d::UnitZ()) * eulerToQuaternion(Vector(90,0,-90));
+
+	    coveredWidth += curWidth;
+	    overallRttWidth += rttWidth;
+	}
+    }
     
-        if(control->graphics)
-          control->graphics->addDrawItems(&draw);
-        
+    rayValues.resize(config.width, 0);
+
+    position = control->nodes->getPosition(attached_node);
+    orientation = control->nodes->getRotation(attached_node);
+
+//     //Drawing Stuff
+//     draw.ptr_draw = (DrawInterface*)this;
+//     item.id = 0;
+//     item.type = DRAW_LINE;
+//     item.draw_state = DRAW_STATE_CREATE;
+//     item.point_size = 1;
+//     item.myColor.r = 1;
+//     item.myColor.g = 0;
+//     item.myColor.b = 0;
+//     item.myColor.a = 1;
+//     item.texture = "";
+//     item.t_width = item.t_height = 0;
+//     item.get_light = 0.0;
+//     
+// 
+//     double rad_step = -config.opening_width / config.width;
+//     double rad_start = config.opening_width / 2.0; //Starting to Left, because 0 is in front and rock convention posive CCW //(M_PI-rad_angle)/2;
+// 
+//     //printf("Rad Start: %f, rad_steps: %f, stepX: %f\n",rad_start,rad_steps,stepX);
+// 
+//     for(i=0; i< config.width; i++){
+// 	Eigen::Vector3d curDir = Eigen::AngleAxisd(rad_start + i * rad_step, Eigen::Vector3d::UnitZ()) * Eigen::Vector3d::UnitX();
+// 	directions.push_back(curDir);
+// 	
+// // 	std::cout << "Dir " << i << " " << curDir.transpose() << std::endl;
+// 	
+// 	item.start = position;
+// 	item.end = position;
+// 	draw.drawItems.push_back(item);
+//     }
+// 
+//     if(control->graphics)
+// 	control->graphics->addDrawItems(&draw);
     
-        assert(rad_steps == data.size());
-      }
+}
+
+RaySensor::~RaySensor(void) {
+    control->graphics->removeDrawItems((DrawInterface*)this);
+    control->dataBroker->unregisterTimedReceiver(this, "*", "*", 
+						"mars_sim/simTimer");
+}
+
+void RaySensor::preGraphicsUpdate(void )
+{
+    for(std::vector<RaySubSensor>::iterator it = subSensors.begin(); it != subSensors.end(); it++)
+    {
+	if(it->gc) {
+	    Eigen::Quaterniond subSensorOrientation = orientation * it->orientation;
+	    it->gc->updateViewportQuat(position.x(), position.y(), position.z(),
+				subSensorOrientation.x(), subSensorOrientation.y(), subSensorOrientation.z(), subSensorOrientation.w());
+	}
     }
+}
 
-    RaySensor::~RaySensor(void) {
-      control->graphics->removeDrawItems((DrawInterface*)this);
-      control->dataBroker->unregisterTimedReceiver(this, "*", "*", 
-                                                   "mars_sim/simTimer");
+std::vector<double> RaySensor::getSensorData() const {
+    return rayValues;
+}
+
+void RaySensor::receiveData(const data_broker::DataInfo &info,
+			    const data_broker::DataPackage &package,
+			    int callbackParam) {
+    CPP_UNUSED(info);
+    CPP_UNUSED(callbackParam);
+    long id;
+    package.get(0, &id);
+
+    if(positionIndices[0] == -1) {
+	positionIndices[0] = package.getIndexByName("position/x");
+	positionIndices[1] = package.getIndexByName("position/y");
+	positionIndices[2] = package.getIndexByName("position/z");
+	rotationIndices[0] = package.getIndexByName("rotation/x");
+	rotationIndices[1] = package.getIndexByName("rotation/y");
+	rotationIndices[2] = package.getIndexByName("rotation/z");
+	rotationIndices[3] = package.getIndexByName("rotation/w");
     }
+    for(int i = 0; i < 3; ++i)
+	package.get(positionIndices[i], &position[i]);
+    
+    package.get(rotationIndices[0], &orientation.x());
+    package.get(rotationIndices[1], &orientation.y());
+    package.get(rotationIndices[2], &orientation.z());
+    package.get(rotationIndices[3], &orientation.w());
 
-    std::vector<double> RaySensor::getSensorData() const {
-      std::vector<double> result;
-      result.resize(data.size());
-      for(unsigned int i=0; i<data.size(); i++) {
-        result[i] = data[i];
-      }
-      return result;
+}
+
+void RaySensor::update(std::vector<draw_item>* drawItems) {
+    
+//     std::cout << "Update Called " << std::endl;
+    
+    
+    int width;
+    int height;
+    
+    double widthStep = 1.0;
+    
+    if(overallRttWidth != config.width)
+    {
+	widthStep = ((double)overallRttWidth) / config.width; 
     }
+    
+//     std::cout << "RttWidht " << overallRttWidth << " widthStep " << widthStep << std::endl;
+    
+    double curX = 0;
+    
+    std::vector<RaySubSensor>::iterator subSensor = subSensors.begin();
+    assert(subSensor != subSensors.end());
+    
+    subSensor->gw->getRTTDepthData(reinterpret_cast<float *>(subSensor->depthBuffer.data()), width, height);
+    
+    int curSubRayStart = 0;
+    int curSubRayEnd = subSensor->rttWidth;
+    
+    for(std::vector<double>::iterator it = rayValues.begin(); it != rayValues.end(); it++)
+    {
+	int x = curX;
+	
+	while(x > curSubRayEnd)
+	{
+// 	    std::cout << "X is " << x << " moving on to next SubSensor " << std::endl;
 
-    void RaySensor::receiveData(const data_broker::DataInfo &info,
-                                const data_broker::DataPackage &package,
-                                int callbackParam) {
-      CPP_UNUSED(info);
-      CPP_UNUSED(callbackParam);
-      long id;
-      package.get(0, &id);
+	    //move on to next camera
+	    subSensor++;
 
-      if(positionIndices[0] == -1) {
-        positionIndices[0] = package.getIndexByName("position/x");
-        positionIndices[1] = package.getIndexByName("position/y");
-        positionIndices[2] = package.getIndexByName("position/z");
-        rotationIndices[0] = package.getIndexByName("rotation/x");
-        rotationIndices[1] = package.getIndexByName("rotation/y");
-        rotationIndices[2] = package.getIndexByName("rotation/z");
-        rotationIndices[3] = package.getIndexByName("rotation/w");
-      }
-      for(int i = 0; i < 3; ++i)
-        package.get(positionIndices[i], &position[i]);
-      package.get(rotationIndices[0], &orientation.x());
-      package.get(rotationIndices[1], &orientation.y());
-      package.get(rotationIndices[2], &orientation.z());
-      package.get(rotationIndices[3], &orientation.w());
+	    assert(subSensor != subSensors.end());
+
+	    curSubRayStart = curSubRayEnd;
+	    
+	    curSubRayEnd += subSensor->rttWidth;
+	    
+	    subSensor->gw->getRTTDepthData(reinterpret_cast<float *>(subSensor->depthBuffer.data()), width, height);
+	    
+	    assert(width == subSensor->rttWidth);
+	}
+	
+	double distance = subSensor->depthBuffer[x - curSubRayStart];
+	
+	*it = distance;
+	
+	curX += widthStep;
+	
+    }
+    
+//     if(!(*drawItems)[0].draw_state) {
+// 	for(size_t i=0; i<rayValues.size(); i++) {
+// 	    (*drawItems)[i].draw_state = DRAW_STATE_UPDATE;
+// 	    (*drawItems)[i].start = position;
+// 	    (*drawItems)[i].end = (orientation * directions[i]);
+// 	    (*drawItems)[i].end *= rayValues[i];
+// 	    (*drawItems)[i].end += (*drawItems)[i].start;
+// 	}
+//     }
   
-      have_update = true;
-    }
+}
 
-    void RaySensor::update(std::vector<draw_item>* drawItems) {
-      unsigned int i;
-      if(config.draw_rays) {
-        if(have_update) {
-          control->nodes->updateRay(attached_node);
-          have_update = false;
-        }
-    
-    
-        if(!(*drawItems)[0].draw_state) {
-          for(i=0; i<data.size(); i++) {
-            (*drawItems)[i].draw_state = DRAW_STATE_UPDATE;
-            (*drawItems)[i].start = position;
-            (*drawItems)[i].end = (orientation * directions[i]);
-            (*drawItems)[i].end *= data[i];
-            (*drawItems)[i].end += (*drawItems)[i].start;
-          }
-        }
-      }
-    }
-
-    BaseConfig* RaySensor::parseConfig(ControlCenter *control,
+BaseConfig* RaySensor::parseConfig(ControlCenter *control,
                                        ConfigMap *config) {
       RayConfig *cfg = new RayConfig;
       unsigned int mapIndex = (*config)["mapIndex"][0].getUInt();
--- sim/src/sensors/RaySensor.h
+++ sim/src/sensors/RaySensor.h
@@ -38,6 +38,8 @@
 #include <mars/utils/Vector.h>
 #include <mars/utils/Quaternion.h>
 #include <mars/interfaces/graphics/draw_structs.h>
+#include <mars/interfaces/graphics/GraphicsUpdateInterface.h>
+#include <mars/interfaces/graphics/GraphicsManagerInterface.h>
 
 namespace mars {
   namespace sim {
@@ -51,7 +53,7 @@ namespace mars {
         pos_offset.setZero();
         ori_offset.setIdentity();
         opening_width=0.5*M_PI;
-        opening_height=0.5*M_PI;
+        opening_height=0;
         attached_node = 0;
         maxDistance = 100.0;
         draw_rays = true;
@@ -69,10 +71,11 @@ namespace mars {
     };
 
     class RaySensor : 
-      public interfaces::BasePolarIntersectionSensor , 
+      public interfaces::BaseNodeSensor,
       public interfaces::SensorInterface, 
       public data_broker::ReceiverInterface,
-      public interfaces::DrawInterface {
+      public interfaces::DrawInterface,
+      public interfaces::GraphicsUpdateInterface {
 
     public:
       static interfaces::BaseSensor* instanciate(interfaces::ControlCenter *control,
@@ -85,6 +88,8 @@ namespace mars {
                                const data_broker::DataPackage &package,
                                int callbackParam);
       virtual void update(std::vector<interfaces::draw_item>* drawItems);
+      
+      virtual void preGraphicsUpdate(void );
 
       static interfaces::BaseConfig* parseConfig(interfaces::ControlCenter *control,
                                                  utils::ConfigMap *config);
@@ -93,12 +98,29 @@ namespace mars {
       const RayConfig& getConfig() const;
 
     private:
-      RayConfig config;
-      std::vector<utils::Vector> directions;
-      bool have_update;
-
-      long positionIndices[3];
-      long rotationIndices[4];
+	struct RaySubSensor
+	{
+	    long cam_window_id;
+	    interfaces::GraphicsWindowInterface *gw;
+	    interfaces::GraphicsCameraInterface *gc;
+	    std::vector<float> depthBuffer;
+	    int rttWidth;
+	    int rttHeight;
+	    double coveredAngle;
+	    utils::Quaternion orientation;
+	};
+	
+	std::vector<RaySubSensor> subSensors;
+	RayConfig config;
+	utils::Vector position;
+	utils::Quaternion orientation;
+	
+	int overallRttWidth;
+	std::vector<double> rayValues;
+	
+	std::vector<utils::Vector> directions;
+	long positionIndices[3];
+	long rotationIndices[4];
     };
 
   } // end of namespace sim
