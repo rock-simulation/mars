ecb63c5865cf52bc79f2a5744380964b2271befc
Constantin Bergatt <constantin.bergatt@dfki.de>
2013-09-23 09:59:30 +0200

    sync_objects:  * extended function "getClosestPoint()" to be able to look for unique matchings (each data point of "data" is only used once)  * if the "RMSE" stagnates, we use the unique matching in order to move on

--- scripts/blender/sync_objects.py
+++ scripts/blender/sync_objects.py
@@ -5,25 +5,34 @@ import bpy
 import mathutils
 
 # Finds the closest points in "data" to every point in "model".
-def getClosestPoints(model, data):
+def getClosestPoints(model, data, bijective=False):
     # init the return value with all zeroes
     closest = numpy.zeros(model.shape)
 
-    # TODO: what happens when there are less "data" points than "model" points!?!?
+    # if there are too much vertices the script is very slow
+    # version 2 is a try to speed things up
+    # TODO: maybe there is a better solution for this case
+    if len(model) > len(data):
+        bijective = False
 
+    # for all vertices in "model"
     for i in range(len(model)):
         # calculate all the distances between the current point of "model" and "data"
         distances = numpy.sum((model[i] - data)**2,axis=-1)**(1./2)
+
         # determine the index of the closest point
         i_min = numpy.argmin(distances)
+
         # set the closest point
         closest[i] = data[i_min]
-        # delete the used point from data (make sure it is only used once)
-        data = numpy.delete(data, i_min, 0)
+
+        # if we want to use each "data" data point only once
+        if bijective:
+            # delete the used point from data (make sure it is only used once)
+            data = numpy.delete(data, i_min, 0)
 
     # return the array of closest points
     return closest
-
 
 # find the mapping (translation and orientation) between "A" and "B"
 def rigid_transform_3D(A, B):
@@ -77,15 +86,20 @@ def main(nodeName1, nodeName2):
     node1 = bpy.data.objects[nodeName1]
     node2 = bpy.data.objects[nodeName2]
 
-    if node1.scale.x != 1.0 and \
-       node1.scale.y != 1.0 and \
-       node1.scale.z != 1.0:
-        print("WARNING! Not all scaling factors of node <%s> are 1.0! Could be problematic!" % nodeName1)
+    # set the rotation_mode to quaternion else the script doesnt work
+    node1.rotation_mode = 'QUATERNION'
+    node2.rotation_mode = 'QUATERNION'
 
-    if node2.scale.x != 1.0 and \
-       node2.scale.y != 1.0 and \
-       node2.scale.z != 1.0:
-        print("WARNING! Not all scaling factors of node <%s> are 1.0! Could be problematic!" % nodeName2)
+
+#    if node1.scale.x != 1.0 and \
+#       node1.scale.y != 1.0 and \
+#       node1.scale.z != 1.0:
+#        print("WARNING! Not all scaling factors of node <%s> are 1.0! Could be problematic!" % nodeName1)
+
+#    if node2.scale.x != 1.0 and \
+#       node2.scale.y != 1.0 and \
+#       node2.scale.z != 1.0:
+#        print("WARNING! Not all scaling factors of node <%s> are 1.0! Could be problematic!" % nodeName2)
 
     # get the pointers to both meshes
     mesh1 = node1.data.vertices
@@ -93,22 +107,34 @@ def main(nodeName1, nodeName2):
 
     # initialize the root mean square error and the iteration counter
     rmse = float("inf")
-    i = 0
+
+    # variable to check if the rmse has changed
+    old_rmse = float("inf")
 
     # do the iterative closest point (ICP) until rmse is small or for
     # a maximal number of iterations
-    while rmse > 0.1 and i < 1:
+    i = 0
 
-        # TODO: ???
-        node1.rotation_quaternion.normalize()
-        node2.rotation_quaternion.normalize()
+    while rmse > 0.1 and i < 50:
+        # convert the list of vertices from local to global coordinates
+        vert1 = numpy.array([node1.rotation_quaternion * \
+                             mathutils.Vector(numpy.multiply(v.co , node1.scale)) + \
+                             node1.location for v in mesh1])
 
-        # TODO: incorporate "scale"
-        vert1 = numpy.array([node1.rotation_quaternion * v.co + node1.location for v in mesh1])
-        vert2 = numpy.array([node2.rotation_quaternion * v.co + node2.location for v in mesh2])
+        vert2 = numpy.array([node2.rotation_quaternion * \
+                             mathutils.Vector(numpy.multiply(v.co , node2.scale)) + \
+                             node2.location for v in mesh2])
 
         # calculate the closest point for each point of vert1 from vert2
-        vert3 = getClosestPoints(vert1, vert2);
+        # if the rmse hasnt changed since last iteration make sure that
+        # every point is only used once
+        if old_rmse != rmse:
+            vert3 = getClosestPoints(vert1, vert2);
+        else:
+            vert3 = getClosestPoints(vert1, vert2, True);
+
+        # store the last rmse as the old one
+        old_rmse = rmse
 
         # recover the transformation
         R, t = rigid_transform_3D(vert1, vert3)
@@ -130,7 +156,7 @@ def main(nodeName1, nodeName2):
 
         # increase the iteration counter
         i += 1
-
+    
     return True
 
 
