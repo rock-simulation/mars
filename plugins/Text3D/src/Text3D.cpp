2a2d8ec1692ea0a9ad4e9388b427023f34116746
Malte Langosz <malte.langosz@dfki.de>
2014-04-04 17:25:49 +0200

    added plugin that creates hud labels from a configuration file

---
+++ plugins/Text3D/src/Text3D.cpp
@@ -0,0 +1,146 @@
+/*
+ *  Copyright 2014, DFKI GmbH Robotics Innovation Center
+ *
+ *  This file is part of the MARS simulation framework.
+ *
+ *  MARS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation, either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  MARS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with MARS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * \file Text3D.cpp
+ * \author Malte (malte.langosz@dfki.de)
+ * \brief A
+ *
+ * Version 0.1
+ */
+
+
+#include "Text3D.h"
+#include <mars/data_broker/DataBrokerInterface.h>
+#include <mars/data_broker/DataPackage.h>
+
+#include <mars/interfaces/graphics/GraphicsManagerInterface.h>
+
+namespace mars {
+  namespace plugins {
+    namespace Text3D {
+
+      using namespace mars::utils;
+      using namespace mars::interfaces;
+
+      Text3D::Text3D(lib_manager::LibManager *theManager)
+        : MarsPluginTemplate(theManager, "Text3D") {
+      }
+  
+      void Text3D::init() {
+
+        // Register for node information:
+        /*
+          std::string groupName, dataName;
+          control->nodes->getDataBrokerNames(id, &groupName, &dataName);
+          control->dataBroker->registerTimedReceiver(this, groupName, dataName, "mars_sim/simTimer", 10, 0);
+        */
+
+        /* get or create cfg_param
+           example = control->cfg->getOrCreateProperty("plugin", "example",
+           0.0, this);
+        */
+        textFactory = libManager->getLibraryAs<osg_text::TextFactoryInterface>("osg_text_factory");
+        if(textFactory) {
+          mars::utils::ConfigMap map;
+          map = mars::utils::ConfigMap::fromYamlFile("Text3DConfig.yml");
+          mars::utils::ConfigVector::iterator it;
+          osg_text::TextInterface *text;
+          if(map.find("Labels") != map.end()) {
+            for(it = map["Labels"].begin(); it!=map["Labels"].end(); ++it) {
+              TextData td;
+              td.name = it->children["name"][0].getString();
+              td.value = it->children["value"][0].getString();
+              td.posX = it->children["posX"][0].getDouble();
+              td.posY = it->children["posY"][0].getDouble();
+              double fixedWidth = it->children["fixedWidth"][0].getDouble();
+              td.text = textFactory->createText(td.value, 40,
+                                                osg_text::Color(1.0, 1.0, 1.0, 1.0),
+                                                td.posX, td.posY);
+              td.text->setBackgroundColor(osg_text::Color(0.0, 0.5, 0.0, 0.5));
+              td.text->setBorderColor(osg_text::Color(1.0, 1.0, 1.0, 0.5));
+              td.text->setBorderWidth(4.0);
+              td.text->setPadding(10., 10., 10., 10.);
+              td.text->setFixedWidth(fixedWidth);
+              example = control->cfg->getOrCreateProperty("Text3D",
+                                                          td.name+"/value",
+                                                          td.value, this);
+              td.vId = example.paramId;
+              example = control->cfg->getOrCreateProperty("Text3D",
+                                                          td.name+"/posX",
+                                                          td.posX, this);
+              td.pxId = example.paramId;
+              example = control->cfg->getOrCreateProperty("Text3D", 
+                                                          td.name+"/posY",
+                                                          td.posY, this);
+              td.pyId = example.paramId;
+              td.hudID = control->graphics->addHUDOSGNode(td.text->getOSGNode());
+              textMap[td.vId] = td;
+              textMap[td.pxId] = td;
+              textMap[td.pyId] = td;
+            }
+          }
+        }
+        else {
+          fprintf(stderr, "can not load osg_text_factory\n");
+        }
+      }
+
+      void Text3D::reset() {
+      }
+
+      Text3D::~Text3D() {
+      }
+
+
+      void Text3D::update(sReal time_ms) {
+      }
+
+      void Text3D::receiveData(const data_broker::DataInfo& info,
+                               const data_broker::DataPackage& package,
+                               int id) {
+        // package.get("force1/x", force);
+      }
+  
+      void Text3D::cfgUpdateProperty(cfg_manager::cfgPropertyStruct _property) {
+        std::map<cfg_manager::cfgParamId, TextData>::iterator it;
+
+        if((it = textMap.find(_property.paramId)) != textMap.end()) {
+          if(it->second.vId == _property.paramId) {
+            it->second.text->setText(_property.sValue);
+          }
+          else if(it->second.pxId == _property.paramId) {
+            it->second.posX = _property.dValue;
+            it->second.text->setPosition(it->second.posX, it->second.posY);
+          }
+          else if(it->second.pyId == _property.paramId) {
+            it->second.posY = _property.dValue;
+            it->second.text->setPosition(it->second.posX, it->second.posY);
+          }
+
+        }
+      }
+
+    } // end of namespace Text3D
+  } // end of namespace plugins
+} // end of namespace mars
+
+DESTROY_LIB(mars::plugins::Text3D::Text3D);
+CREATE_LIB(mars::plugins::Text3D::Text3D);
---
+++ plugins/Text3D/src/Text3D.h
@@ -0,0 +1,111 @@
+/*
+ *  Copyright 2014, DFKI GmbH Robotics Innovation Center
+ *
+ *  This file is part of the MARS simulation framework.
+ *
+ *  MARS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation, either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  MARS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with MARS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * \file Text3D.h
+ * \author Malte (malte.langosz@dfki.de)
+ * \brief A
+ *
+ * Version 0.1
+ */
+
+#ifndef MARS_PLUGINS_TEXT3D_H
+#define MARS_PLUGINS_TEXT3D_H
+
+#ifdef _PRINT_HEADER_
+  #warning "Text3D.h"
+#endif
+
+// set define if you want to extend the gui
+//#define PLUGIN_WITH_MARS_GUI
+#include <mars/interfaces/sim/MarsPluginTemplate.h>
+#include <mars/interfaces/MARSDefs.h>
+#include <mars/data_broker/ReceiverInterface.h>
+#include <mars/cfg_manager/CFGManagerInterface.h>
+//#include <mars/interfaces/graphics/GraphicsUpdateInterface.h>
+
+#include <string>
+
+#include <mars/osg_text_factory/TextFactoryInterface.h>
+
+namespace mars {
+
+  namespace lib_manager {
+    class LibManager;
+  }
+
+  namespace plugins {
+    namespace Text3D {
+
+      struct TextData {
+        osg_text::TextInterface *text;
+        std::string name;
+        std::string value;
+        unsigned long hudID;
+        double posX, posY;
+        cfg_manager::cfgParamId vId, pxId, pyId;
+      };
+
+      // inherit from MarsPluginTemplateGUI for extending the gui
+      class Text3D: public mars::interfaces::MarsPluginTemplate,
+                    public mars::data_broker::ReceiverInterface,
+                    // for gui
+                    //public mars::main_gui::MenuInterface,
+                    public mars::cfg_manager::CFGClient {
+
+      public:
+        Text3D(mars::lib_manager::LibManager *theManager);
+        ~Text3D();
+
+        // LibInterface methods
+        int getLibVersion() const
+        { return 1; }
+        const std::string getLibName() const
+        { return std::string("Text3D"); }
+        CREATE_MODULE_INFO();
+
+        // MarsPlugin methods
+        void init();
+        void reset();
+        void update(mars::interfaces::sReal time_ms);
+
+        // DataBrokerReceiver methods
+        virtual void receiveData(const data_broker::DataInfo &info,
+                                 const data_broker::DataPackage &package,
+                                 int callbackParam);
+        // CFGClient methods
+        virtual void cfgUpdateProperty(cfg_manager::cfgPropertyStruct _property);
+
+        //virtual void preGraphicsUpdate(void);
+
+        // Text3D methods
+
+      private:
+        cfg_manager::cfgPropertyStruct example;
+        osg_text::TextFactoryInterface *textFactory;
+        std::map<cfg_manager::cfgParamId, TextData> textMap;
+
+      }; // end of class definition Text3D
+
+    } // end of namespace Text3D
+  } // end of namespace plugins
+} // end of namespace mars
+
+#endif // MARS_PLUGINS_TEXT3D_H
