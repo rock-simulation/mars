48445c3e679086eac1768649a7871b65c074542e
Kai von Szadkowski <kavo01@dfki.de>
2014-05-22 17:30:17 +0200

    obstacle_generator: fixed slope angle issues, added support platform

--- plugins/obstacle_generator/src/ObstacleGenerator.h
+++ plugins/obstacle_generator/src/ObstacleGenerator.h
@@ -93,6 +93,7 @@ namespace mars {
         std::map<cfg_manager::cfgParamId, std::string> paramIds;
         std::vector<mars::interfaces::NodeId> oldNodeIDs;
         std::map<std::string, std::string> textures;
+        bool support_platform;
         double sigma;
         double random_normal_number(double mean, double std, double min, double max);
         double random_number(double min, double max);
