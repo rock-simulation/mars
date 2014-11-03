8ddc8c8bdc1590586facf59ae5858166e2b770a2
Kai von Szadkowski <kavo01@dfki.de>
2014-11-03 16:51:06 +0100

    add addEntity(SimEntity*), formatting

--- sim/src/core/EntityManager.cpp
+++ sim/src/core/EntityManager.cpp
@@ -20,209 +20,212 @@
 
 #include "EntityManager.h"
 #include "SimEntity.h"
+#include <mars/utils/ConfigData.h>
 #include <mars/interfaces/graphics/GraphicsManagerInterface.h>
+#include <mars/plugins/entity_generation/EntityFactoryInterface.h>
 #include <mars/utils/MutexLocker.h>
 
 #include <iostream>
+#include <string>
 
 namespace mars {
-  namespace sim {
+namespace sim {
 
-    using namespace utils;
-    using namespace interfaces;
+using namespace utils;
+using namespace interfaces;
 
-    EntityManager::EntityManager(ControlCenter* c)
-    {
+EntityManager::EntityManager(ControlCenter* c) {
 
-      control = c;
-      next_entity_id = 1;
-      if(control->graphics)
-        control->graphics->addEventClient((GraphicsEventClient*)this);
+  control = c;
+  next_entity_id = 1;
+  if (control->graphics)
+    control->graphics->addEventClient((GraphicsEventClient*) this);
+}
+
+unsigned long EntityManager::addEntity(const std::string &name) {
+  unsigned long id = 0;
+  MutexLocker locker(&iMutex);
+  entities[id = getNextId()] = new SimEntity(name);
+  return id;
+}
+
+unsigned long EntityManager::addEntity(SimEntity* entity) {
+  unsigned long id = 0;
+  MutexLocker locker(&iMutex);
+  entities[id = getNextId()] = entity;
+  return id;
+}
+
+void EntityManager::addNode(const std::string& entityName, long unsigned int nodeId,
+    const std::string& nodeName) {
+  SimEntity *entity = 0;
+  //iterate over all robots to find the robot with the given name
+  for (std::map<unsigned long, SimEntity*>::iterator iter = entities.begin();
+      iter != entities.end(); ++iter) {
+    if (iter->second->getName() == entityName) {
+      entity = iter->second;
+      break;
     }
+  }
+  if (entity) {
+    MutexLocker locker(&iMutex);
+    entity->addNode(nodeId, nodeName);
+  }
+}
 
-
-    unsigned long EntityManager::addEntity(const std::string &name)
-    {
-      unsigned long id = 0;
+void EntityManager::addMotor(const std::string& entityName, long unsigned int motorId,
+    const std::string& motorName) {
+  //iterate over all robots to find the robot with the given name
+  for (std::map<unsigned long, SimEntity*>::iterator iter = entities.begin();
+      iter != entities.end(); ++iter) {
+    if (iter->second->getName() == entityName) {
       MutexLocker locker(&iMutex);
-      simRobots[id = getNextId()] = new SimEntity(name);
-      return id;
+      iter->second->addMotor(motorId, motorName);
+      break;
     }
+  }
+}
 
-
-    void EntityManager::addNode(const std::string& entityName, long unsigned int nodeId, const std::string& nodeName)
-    {
-      SimEntity *entity = 0;
-      //iterate over all robots to find the robot with the given name
-      for(std::map<unsigned long, SimEntity*>::iterator iter = simRobots.begin(); iter != simRobots.end(); ++iter){
-        if(iter->second->getName() == entityName){
-          entity = iter->second;
-          break;
-        }
-      }
-      if(entity){
-        MutexLocker locker(&iMutex);
-        entity->addNode(nodeId, nodeName);
-      }
-    }
-
-    void EntityManager::addMotor(const std::string& entityName, long unsigned int motorId, const std::string& motorName)
-    {
-      //iterate over all robots to find the robot with the given name
-      for(std::map<unsigned long, SimEntity*>::iterator iter = simRobots.begin(); iter != simRobots.end(); ++iter){
-        if(iter->second->getName() == entityName){
-          MutexLocker locker(&iMutex);
-          iter->second->addMotor(motorId, motorName);
-          break;
-        }
-      }
-    }
-
-    void EntityManager::addJoint(const std::string& entityName, long unsigned int jointId, const std::string& jointName)
-    {
-      //iterate over all robots to find the robot with the given name
-      for(std::map<unsigned long, SimEntity*>::iterator iter = simRobots.begin(); iter != simRobots.end(); ++iter){
-        if(iter->second->getName() == entityName){
-          MutexLocker locker(&iMutex);
-          iter->second->addJoint(jointId, jointName);
-          break;
-        }
-      }
-    }
-
-
-    void EntityManager::addController(const std::string& entityName, long unsigned int controllerId)
-    {
-      //iterate over all robots to find the robot with the given name
-      for(std::map<unsigned long, SimEntity*>::iterator iter = simRobots.begin(); iter != simRobots.end(); ++iter){
-        if(iter->second->getName() == entityName){
-          MutexLocker locker(&iMutex);
-          iter->second->addController(controllerId);
-          break;
-        }
-      }
-    }
-
-
-    void EntityManager::selectEvent(long unsigned int id, bool mode)
-    {
-      //the node was selected
-      if(true == mode){
-        //go over all robots as we don't know which robot the node belongs to
-        for(std::map<unsigned long, SimEntity*>::iterator iter = simRobots.begin(); iter != simRobots.end(); ++iter){
-          //select returns true if the node belongs to the robot
-          if(iter->second->select(id)){
-            //TODO <jonas.peter@dfki.de> notify about selection change only if new selection
-            std::cout << "robot has been selected: " << iter->second->getName() << std::endl;
-            //TODO <jonas.peter@dfki.de> notify clients about selection change
-          }
-        }
-      }
-      //TODO <jonas.peter@dfki.de> handle deselection
-    }
-
-    SimEntity* EntityManager::getEntity(const std::string& name)
-    {
-      for (std::map<unsigned long, SimEntity*>::iterator iter = simRobots.begin(); iter != simRobots.end(); ++iter) {
-        if (iter->second->getName() == name) {
-          return iter->second;
-        }
-      }
-      return 0;
-    }
-
-    SimEntity* EntityManager::getEntity(long unsigned int id)
-    {
-      //TODO replace with find
-      for(std::map<unsigned long, SimEntity*>::iterator iter = simRobots.begin();
-          iter != simRobots.end(); ++iter){
-        if(iter->first == id){
-          return iter->second;
-        }
-      }
-      return 0;
-    }
-
-    long unsigned int EntityManager::getEntityNode(const std::string& entityName,
-                                                   const std::string& nodeName)
-    {
+void EntityManager::addJoint(const std::string& entityName, long unsigned int jointId,
+    const std::string& jointName) {
+  //iterate over all robots to find the robot with the given name
+  for (std::map<unsigned long, SimEntity*>::iterator iter = entities.begin();
+      iter != entities.end(); ++iter) {
+    if (iter->second->getName() == entityName) {
       MutexLocker locker(&iMutex);
-      SimEntity *entity = getEntity(entityName);
-      unsigned long node = 0;
-      if(entity){
-        node = entity->getNode(nodeName);
-      }
-      return node;
+      iter->second->addJoint(jointId, jointName);
+      break;
     }
+  }
+}
 
-    long unsigned int EntityManager::getEntityMotor(const std::string& entityName, const std::string& motorName)
-    {
-      //not sure if a mutex lock is needed here
+void EntityManager::addController(const std::string& entityName, long unsigned int controllerId) {
+  //iterate over all robots to find the robot with the given name
+  for (std::map<unsigned long, SimEntity*>::iterator iter = entities.begin();
+      iter != entities.end(); ++iter) {
+    if (iter->second->getName() == entityName) {
       MutexLocker locker(&iMutex);
-      SimEntity *entity = getEntity(entityName);
-      unsigned long motor = 0;
-      if (entity) {
-        motor = entity->getMotor(motorName);
-      }
-      return motor;
+      iter->second->addController(controllerId);
+      break;
     }
+  }
+}
 
-    std::vector<unsigned long> EntityManager::getEntityControllerList(const std::string &entityName)
-    {
-      //not sure if a mutex lock is needed here
-      MutexLocker locker(&iMutex);
-      SimEntity *entity = getEntity(entityName);
-      if (entity) {
-        return entity->getControllerList();
-      }
-      return std::vector<unsigned long>();
-    }
-
-    long unsigned int EntityManager::getEntityJoint(const std::string& entityName, const std::string& jointName)
-    {
-      //not sure if a mutex lock is needed here
-      MutexLocker locker(&iMutex);
-      SimEntity *entity = getEntity(entityName);
-      unsigned long joint = 0;
-      if (entity) {
-        joint = entity->getJoint(jointName);
-      }
-      return joint;
-    }
-
-
-    void EntityManager::printEntityNodes(const std::string& entityName)
-    {
-      for(std::map<unsigned long, SimEntity*>::iterator iter = simRobots.begin(); iter != simRobots.end(); ++iter){
-        if(iter->second->getName() == entityName){
-          std::cout << "printing entity with id: " << iter->first << std::endl;
-          iter->second->printNodes();    
-          break;
-        }
+void EntityManager::selectEvent(long unsigned int id, bool mode) {
+  //the node was selected
+  if (true == mode) {
+    //go over all robots as we don't know which robot the node belongs to
+    for (std::map<unsigned long, SimEntity*>::iterator iter = entities.begin();
+        iter != entities.end(); ++iter) {
+      //select returns true if the node belongs to the robot
+      if (iter->second->select(id)) {
+        //TODO <jonas.peter@dfki.de> notify about selection change only if new selection
+        std::cout << "robot has been selected: " << iter->second->getName() << std::endl;
+        //TODO <jonas.peter@dfki.de> notify clients about selection change
       }
     }
+  }
+  //TODO <jonas.peter@dfki.de> handle deselection
+}
 
-    void EntityManager::printEntityMotors(const std::string& entityName)
-    {
-      for(std::map<unsigned long, SimEntity*>::iterator iter = simRobots.begin(); iter != simRobots.end(); ++iter){
-        if(iter->second->getName() == entityName){
-          std::cout << "printing entity with id: " << iter->first << std::endl;
-          iter->second->printMotors();    
-          break;
-        }
-      }
+SimEntity* EntityManager::getEntity(const std::string& name) {
+  for (std::map<unsigned long, SimEntity*>::iterator iter = entities.begin();
+      iter != entities.end(); ++iter) {
+    if (iter->second->getName() == name) {
+      return iter->second;
     }
+  }
+  return 0;
+}
 
-    void EntityManager::printEntityControllers(const std::string& entityName)
-    {
-      for(std::map<unsigned long, SimEntity*>::iterator iter = simRobots.begin(); iter != simRobots.end(); ++iter){
-        if(iter->second->getName() == entityName){
-          std::cout << "printing entity with id: " << iter->first << std::endl;
-          iter->second->printControllers();    
-          break;
-        }
-      }
+SimEntity* EntityManager::getEntity(long unsigned int id) {
+  //TODO replace with find
+  for (std::map<unsigned long, SimEntity*>::iterator iter = entities.begin();
+      iter != entities.end(); ++iter) {
+    if (iter->first == id) {
+      return iter->second;
     }
+  }
+  return 0;
+}
 
-  } // end of namespace sim
+long unsigned int EntityManager::getEntityNode(const std::string& entityName,
+    const std::string& nodeName) {
+  MutexLocker locker(&iMutex);
+  SimEntity *entity = getEntity(entityName);
+  unsigned long node = 0;
+  if (entity) {
+    node = entity->getNode(nodeName);
+  }
+  return node;
+}
+
+long unsigned int EntityManager::getEntityMotor(const std::string& entityName,
+    const std::string& motorName) {
+  //not sure if a mutex lock is needed here
+  MutexLocker locker(&iMutex);
+  SimEntity *entity = getEntity(entityName);
+  unsigned long motor = 0;
+  if (entity) {
+    motor = entity->getMotor(motorName);
+  }
+  return motor;
+}
+
+std::vector<unsigned long> EntityManager::getEntityControllerList(const std::string &entityName) {
+  //not sure if a mutex lock is needed here
+  MutexLocker locker(&iMutex);
+  SimEntity *entity = getEntity(entityName);
+  if (entity) {
+    return entity->getControllerList();
+  }
+  return std::vector<unsigned long>();
+}
+
+long unsigned int EntityManager::getEntityJoint(const std::string& entityName,
+    const std::string& jointName) {
+  //not sure if a mutex lock is needed here
+  MutexLocker locker(&iMutex);
+  SimEntity *entity = getEntity(entityName);
+  unsigned long joint = 0;
+  if (entity) {
+    joint = entity->getJoint(jointName);
+  }
+  return joint;
+}
+
+void EntityManager::printEntityNodes(const std::string& entityName) {
+  for (std::map<unsigned long, SimEntity*>::iterator iter = entities.begin();
+      iter != entities.end(); ++iter) {
+    if (iter->second->getName() == entityName) {
+      std::cout << "printing entity with id: " << iter->first << std::endl;
+      iter->second->printNodes();
+      break;
+    }
+  }
+}
+
+void EntityManager::printEntityMotors(const std::string& entityName) {
+  for (std::map<unsigned long, SimEntity*>::iterator iter = entities.begin();
+      iter != entities.end(); ++iter) {
+    if (iter->second->getName() == entityName) {
+      std::cout << "printing entity with id: " << iter->first << std::endl;
+      iter->second->printMotors();
+      break;
+    }
+  }
+}
+
+void EntityManager::printEntityControllers(const std::string& entityName) {
+  for (std::map<unsigned long, SimEntity*>::iterator iter = entities.begin();
+      iter != entities.end(); ++iter) {
+    if (iter->second->getName() == entityName) {
+      std::cout << "printing entity with id: " << iter->first << std::endl;
+      iter->second->printControllers();
+      break;
+    }
+  }
+}
+
+} // end of namespace sim
 } // end of namespace mars
--- sim/src/core/EntityManager.h
+++ sim/src/core/EntityManager.h
@@ -20,10 +20,10 @@
 
 /**
  * \file EntityManager.h
- * \author Jonas Peter
- * \brief "EntityManager" is the class that manages information about entire entities
+ * \author Jonas Peter, Kai von Szadkowski
+ * \brief "EntityManager" is the class that manages information about simulation entities
  * 
- * The class can be used to acces information about nodes and the postion of the robot
+ * The class can be used to access information about nodes and the position of the robot
  * to use pass robot name as parameter to the SimulatorInterface::loadScene function
  *
  * TODO delete robots after use
@@ -41,73 +41,81 @@
 #include <mars/interfaces/graphics/GraphicsEventClient.h>
 #include <mars/interfaces/sim/EntityManagerInterface.h>
 #include <mars/utils/Mutex.h>
+#include <mars/utils/ConfigData.h>
 
 namespace mars {
-  namespace sim {
+namespace sim {
 
-    class SimEntity;
-    /*get notifications 
-     * about selection changes*/
-    class EntityManager: public interfaces::GraphicsEventClient,
-                         public interfaces::EntityManagerInterface
-    {
-    public:
-      EntityManager(interfaces::ControlCenter *c);
-      /**creates a new robot with the given name and returns its id*/
-      virtual unsigned long addEntity(const std::string &name);
-    
-      /**adds a node and maps the nodeId to the name*/
-      virtual void addNode(const std::string &entityName, unsigned long nodeId, const std::string &nodeName);
-    
-      /**adds a motor to the entity and maps the motorId to its name*/
-      virtual void addMotor(const std::string &entityName, unsigned long motorId, const std::string &motorName);
-    
-      /**adds a controller id to the controller list*/
-      virtual void addController(const std::string &entityName, unsigned long controllerId);
-    
-      /**adds a joint to the entity and maps the jointId to its name*/
-      virtual void addJoint(const std::string &entityName, unsigned long jointId, const std::string &jointName);
-    
-      /**returns the entity with the given name
-       */
-      virtual SimEntity* getEntity(const std::string &name);
-    
-      /**returns the entity with the given id
-       */
-      virtual SimEntity* getEntity(unsigned long id);
-     
-      virtual unsigned long getEntityNode(const std::string &entityName, const std::string &nodeName);
-     
-      virtual unsigned long getEntityMotor(const std::string &entityName, const std::string &motorName);
-     
-      virtual std::vector<unsigned long> getEntityControllerList(const std::string &entityName);
-     
-      /**returns the node of the given entity; returns 0 if the entity or the node don't exist*/
-      virtual unsigned long getEntityJoint(const std::string &entityName, const std::string &jointName);
-     
-      //from graphics event client
-      virtual void selectEvent(unsigned long id, bool mode);
-     
-      //debug functions
-      virtual void printEntityNodes(const std::string &entityName);
-      virtual void printEntityMotors(const std::string &entityName);
-      virtual void printEntityControllers(const std::string &entityName);
-    
-    private: 
-      interfaces::ControlCenter *control;
-      /**the id assigned to the next created entity; use getNextId function*/
-      unsigned long next_entity_id;
-      std::map<unsigned long, SimEntity*> simRobots;
-    
-      /**returns the id to be assigned to the next entity*/
-      unsigned long getNextId(){ return next_entity_id++;}
-    
-      //! a mutex fot the sensor containters
-      mutable utils::Mutex iMutex;
+class SimEntity;
 
-    };
+/*get notifications
+ * about selection changes*/
+class EntityManager: public interfaces::GraphicsEventClient,
+    public interfaces::EntityManagerInterface {
+  public:
+  EntityManager(interfaces::ControlCenter *c);
+  /**creates a new entity with the given name and returns its id*/
+  virtual unsigned long addEntity(const std::string &name);
 
-  } // end of namespace sim
+  virtual unsigned long addEntity(SimEntity* entity);
+
+  /**adds a node and maps the nodeId to the name*/
+  virtual void addNode(const std::string &entityName, unsigned long nodeId,
+      const std::string &nodeName);
+
+  /**adds a motor to the entity and maps the motorId to its name*/
+  virtual void addMotor(const std::string &entityName, unsigned long motorId,
+      const std::string &motorName);
+
+  /**adds a controller id to the controller list*/
+  virtual void addController(const std::string &entityName, unsigned long controllerId);
+
+  /**adds a joint to the entity and maps the jointId to its name*/
+  virtual void addJoint(const std::string &entityName, unsigned long jointId,
+      const std::string &jointName);
+
+  /**returns the entity with the given name
+   */
+  virtual SimEntity* getEntity(const std::string &name);
+
+  /**returns the entity with the given id
+   */
+  virtual SimEntity* getEntity(unsigned long id);
+
+  virtual unsigned long getEntityNode(const std::string &entityName, const std::string &nodeName);
+
+  virtual unsigned long getEntityMotor(const std::string &entityName, const std::string &motorName);
+
+  virtual std::vector<unsigned long> getEntityControllerList(const std::string &entityName);
+
+  /**returns the node of the given entity; returns 0 if the entity or the node don't exist*/
+  virtual unsigned long getEntityJoint(const std::string &entityName, const std::string &jointName);
+
+  //from graphics event client
+  virtual void selectEvent(unsigned long id, bool mode);
+
+  //debug functions
+  virtual void printEntityNodes(const std::string &entityName);
+  virtual void printEntityMotors(const std::string &entityName);
+  virtual void printEntityControllers(const std::string &entityName);
+
+  private:
+  interfaces::ControlCenter *control;
+  /**the id assigned to the next created entity; use getNextId function*/
+  unsigned long next_entity_id;
+  std::map<unsigned long, SimEntity*> entities;
+
+  /**returns the id to be assigned to the next entity*/
+  unsigned long getNextId() {
+    return next_entity_id++;
+  }
+
+  // a mutex for the sensor containers
+  mutable utils::Mutex iMutex;
+
+};
+
+} // end of namespace sim
 } // end of namespace mars
 
 #endif // ENTITYMANAGER_H
