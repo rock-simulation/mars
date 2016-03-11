b31126bb8064ceda26e8c4debac9c50b9f2dd384
Kai von Szadkowski <kavonszadkowski@KSZADKOWSKI-U.dfki.uni-bremen.de>
2016-03-11 17:53:46 +0100

    Allow entities to be parented to other entities

--- sim/src/core/SimEntity.cpp
+++ sim/src/core/SimEntity.cpp
@@ -29,6 +29,7 @@
 #include <mars/interfaces/sim/NodeManagerInterface.h>
 #include <mars/interfaces/sim/JointManagerInterface.h>
 #include <mars/interfaces/sim/MotorManagerInterface.h>
+#include <mars/interfaces/sim/EntityManagerInterface.h>
 #include <iterator> // ostream_iterator
 
 namespace mars {
@@ -175,23 +176,17 @@ namespace mars {
     }
 
     void SimEntity::setInitialPose(bool reset) {
-      bool worldAnchor = false;
-      if(!control) return;
-      if(config.find("rootNode") != config.end()) {
-        if(config.find("anchor") != config.end()) {
-          if((std::string)config["anchor"] == "world") {
-            worldAnchor = true;
-          }
-        }
+
+      if(control && (config.find("rootNode") != config.end())) {
         NodeId id = getNode((std::string)config["rootNode"]);
-        NodeData myNode = control->nodes->getFullNode(id);
+        NodeData rootNode = control->nodes->getFullNode(id);
         utils::Quaternion tmpQ(1, 0, 0, 0);
         utils::Vector tmpV;
         if(config.find("position") != config.end()) {
-          myNode.pos.x() = config["position"][0];
-          myNode.pos.y() = config["position"][1];
-          myNode.pos.z() = config["position"][2];
-          control->nodes->editNode(&myNode, EDIT_NODE_POS | EDIT_NODE_MOVE_ALL);
+          rootNode.pos.x() = config["position"][0];
+          rootNode.pos.y() = config["position"][1];
+          rootNode.pos.z() = config["position"][2];
+          control->nodes->editNode(&rootNode, EDIT_NODE_POS | EDIT_NODE_MOVE_ALL);
         }
         if(config.find("rotation") != config.end()) {
           // check if euler angles or quaternion is provided; rotate around z
@@ -213,10 +208,11 @@ namespace mars {
             tmpQ.w() = (sReal)config["rotation"][0];
             break;
           }
-          myNode.rot = tmpQ;
-          control->nodes->editNode(&myNode, EDIT_NODE_ROT | EDIT_NODE_MOVE_ALL);
+          rootNode.rot = tmpQ;
+          control->nodes->editNode(&rootNode, EDIT_NODE_ROT | EDIT_NODE_MOVE_ALL);
         }
-        if(worldAnchor) {
+        // check if there is an anchor / parent (anchor is deprecated...)
+        if(config.find("anchor") != config.end() || config.find("parent") != config.end()) {
           if (reset) {
             fprintf(stderr, "Resetting initial entity pose.\n");
             std::map<unsigned long, std::string>::iterator it;
@@ -234,9 +230,34 @@ namespace mars {
             else fprintf(stderr, "Could not reset anchor of entity %s.\n", name.c_str());
           }
           else {
+            std::string parentname;
+            if (config.find("parent") != config.end())
+              parentname = (std::string)config["parent"];
+            if (config.find("anchor") != config.end())  // backwards compatibility
+              parentname = (std::string)config["anchor"];
+            unsigned long parentid;
+            if (parentname == "world") {
+              parentid = 0;
+            } else {  // if entity gets attached to other entity
+              std::string entityname, linkname;
+              unsigned int splitsignpos = parentname.find("::");
+              if (splitsignpos >= 0) {
+                entityname = parentname.substr(0, splitsignpos);
+                linkname = parentname.substr(splitsignpos+2);
+                SimEntity* parententity = control->entities->getEntity(entityname);
+                if (parententity != 0) {
+                  unsigned long linkid = parententity->getNode(linkname);
+                  if (linkid > 0) {
+                    parentid = linkid;
+                  } else
+                      fprintf(stderr, "No valid link id in parent entity.\n");
+                } else
+                   fprintf(stderr, "No valid entity in scene.\n");
+              }
+            }
             JointData anchorjoint;
             anchorjoint.nodeIndex1 = id;
-            anchorjoint.nodeIndex2 = 0;
+            anchorjoint.nodeIndex2 = parentid;
             anchorjoint.type = JOINT_TYPE_FIXED;
             anchorjoint.name = "anchor_"+name;
             JointId anchorJointId = control->joints->addJoint(&anchorjoint);
