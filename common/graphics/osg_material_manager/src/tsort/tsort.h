d84d4660dd11e20eb7689b978e67d7537b3f8491
Ole Schwiegert <ole.schwiegert@uni-bremen.de>
2017-08-22 11:35:43 +0200

    Add tsort library

---
+++ common/graphics/osg_material_manager/src/tsort/tsort.h
@@ -0,0 +1,25 @@
+/* tsort - topological sort.
+   Copyright (C) 1998-2013 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Mark Kettenis <kettenis@phys.uva.nl>.  */
+
+/* The topological sort is done according to Algorithm T (Topological
+   sort) in Donald E. Knuth, The Art of Computer Programming, Volume
+   1/Fundamental Algorithms, page 262.  */
+
+void add_relation(unsigned long id1, unsigned long id2);
+int tsort (void);
+unsigned long* get_sorted_ids(void);
