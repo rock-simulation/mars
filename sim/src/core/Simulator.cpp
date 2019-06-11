47ad126161c129bd59df21dd2874817daeb80b1a
Alexander Dettmann <Alexander.Dettmann@dfki.de>
2019-06-11 17:11:04 +0200

    getTime cfgUseNow bug fixed

--- sim/src/core/Simulator.cpp
+++ sim/src/core/Simulator.cpp
@@ -1400,7 +1400,7 @@ namespace mars {
       unsigned long returnTime;
       getTimeMutex.lock();
       if(cfgUseNow.bValue) {
-        returnTime = realStartTime+dbSimTimePackage[0].d;
+        returnTime = utils::getTime();
       }
       else {
         returnTime = realStartTime+dbSimTimePackage[0].d;
