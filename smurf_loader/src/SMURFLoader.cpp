7210046c251c4e74a953aacb8e16761d91cb4f3f
Malte Langosz <malte.langosz@dfki.de>
2022-04-03 18:07:30 +0200

    Add parsing of animation configuration

--- smurf_loader/src/SMURFLoader.cpp
+++ smurf_loader/src/SMURFLoader.cpp
@@ -486,20 +486,26 @@ namespace mars {
 	      control->cfg->setPropertyValue("Scene", "skydome_enabled", "value",
 					     true);
 	    }
+      if(envmap.hasKey("animation")) {
+        if(envmap["animations"].hasKey("config_path")) {
+          control->cfg->createParam("Scene","animations_path", cfg_manager::stringParam);
+          control->cfg->setPropertyValue("Scene", "animations_path", "value", std::string(envmap["animations"]["config_path"]));
+        }
+      }
 	    if (envmap.hasKey("terrain")) {
 	      control->cfg->createParam("Scene","terrain_path", cfg_manager::stringParam);
 	      control->cfg->setPropertyValue("Scene", "terrain_path", "value", std::string(envmap["terrain"]["path"]));
 	      if(envmap["terrain"].hasKey("mesh")) {
-		control->cfg->createParam("Scene","mesh", cfg_manager::stringParam);
-		control->cfg->setPropertyValue("Scene", "mesh", "value", std::string(envmap["terrain"]["mesh"]));
+          control->cfg->createParam("Scene","mesh", cfg_manager::stringParam);
+          control->cfg->setPropertyValue("Scene", "mesh", "value", std::string(envmap["terrain"]["mesh"]));
 	      }
 	      if(envmap["terrain"].hasKey("mesh_material")) {
-		control->cfg->createParam("Scene","mesh_material", cfg_manager::stringParam);
-		control->cfg->setPropertyValue("Scene", "mesh_material", "value", std::string(envmap["terrain"]["mesh_material"]));
+          control->cfg->createParam("Scene","mesh_material", cfg_manager::stringParam);
+          control->cfg->setPropertyValue("Scene", "mesh_material", "value", std::string(envmap["terrain"]["mesh_material"]));
 	      }
 	      if(envmap["terrain"].hasKey("mesh_scale")) {
-		control->cfg->createParam("Scene","mesh_scale", cfg_manager::doubleParam);
-		control->cfg->setPropertyValue("Scene", "mesh_scale", "value", (double)(envmap["terrain"]["mesh_scale"]));
+          control->cfg->createParam("Scene","mesh_scale", cfg_manager::doubleParam);
+          control->cfg->setPropertyValue("Scene", "mesh_scale", "value", (double)(envmap["terrain"]["mesh_scale"]));
 	      }
 	    }
 	    if(control->graphics) {
